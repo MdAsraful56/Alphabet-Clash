83022cc38f83d410071fc16e22533ef3b658037d
MdAsraful56 <work.mdasraful56@gmail.com>
2024-12-17 16:46:26 +0600

    complite

--- script/script.js
+++ script/script.js
@@ -9,18 +9,85 @@
 
 function play(){
     hideElementById('home');
+    hideElementById('final-score');
     showElementById('play');
+
+    setTextElementValueById('life', 5);
+    setTextElementValueById('score', 0);
+
     continusGame();
 }
 
 
 function continusGame(){
     const alphabet = getRandomAlphabet();
-    // console.log(alphabet);
 
     const currentAlphabet =document.getElementById('current-alphabet');
     currentAlphabet.innerText = alphabet;
 
-
     setBgColorById(alphabet);
+}
+
+
+document.addEventListener('keyup', function(event){
+    const playerKeyPressed = event.key;
+
+    // stop game 
+    if (playerKeyPressed = 'Escape') {
+        gameOver();
+    }
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    const currentAlphabetTarget = currentAlphabet.innerText;
+
+    if (playerKeyPressed === currentAlphabetTarget) {
+
+        const score = getTextElementValueById('score');
+        const NewScore = score + 1;
+        setTextElementValueById('score', NewScore);
+
+
+// -----------non function useing proseses--------------------
+        // const score = document.getElementById('score');
+        // const scoreText = score.innerText;
+        // const scoreNumber = parseInt(scoreText);
+        
+        // const NewScore = scoreNumber + 1;
+        // score.innerText = NewScore;
+
+        removeBgColorById(playerKeyPressed);
+        continusGame();
+    } else {
+
+        const life = getTextElementValueById('life');
+        const Newlife = life - 1;
+        setTextElementValueById('life', Newlife);
+
+        if (Newlife === 0) {
+            gameOver();
+        }
+// -----------non function useing proseses--------------------
+        // const life = document.getElementById('life');
+        // const lifeText = life.innerText;
+        // const lifeNumber = parseInt(lifeText);
+        
+        // const Newlife = lifeNumber - 1;
+        // life.innerText = Newlife;
+    }
+})
+
+
+function gameOver(){
+    hideElementById('play');
+    // showElementById('score');
+    showElementById('final-score');
+
+    const lastScore = document.getElementById('score').innerText;
+    // console.log(lastScore.innerText);
+    setTextElementValueById('game-score', lastScore);
+
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBgColorById(currentAlphabet);
 }
--- script/utlity.js
+++ script/utlity.js
@@ -8,15 +8,37 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
+// -------------------------------------------------------
+
 function setBgColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-500');
 }
 
+function removeBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-500');
+}
+
+// ----------------------------------------------------------
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+// ------------------------------------------------------------
+
 function getRandomAlphabet() {
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-
 
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
@@ -24,4 +46,12 @@ function getRandomAlphabet() {
     const alphabet = alphabets[index];
     // console.log(index, alphabet);
     return alphabet;
+}
+
+
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
